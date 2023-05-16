ae58cb8e7412f0d5f157678626a0730416672522
kennethmanuel <kennethlieyanto99@gmail.com>
2023-05-16 20:34:06 +0700

    Trying to make the camera for classifier work

--- src/app/tabClassify/tabClassify.page.ts
+++ src/app/tabClassify/tabClassify.page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { Capacitor, CapacitorCookies } from '@capacitor/core';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabClassify',
@@ -7,6 +11,37 @@ import { Component } from '@angular/core';
 })
 export class TabClassifyPage {
 
+  image: any;
+
   constructor() { }
+
+  async takePicture() {
+    try {
+      if (Capacitor.getPlatform() != 'web') await Camera.requestPermissions();
+      const image = await Camera.getPhoto({
+        quality: 100,
+        source: CameraSource.Prompt,
+        width: 600,
+        resultType: CameraResultType.DataUrl
+      });
+      console.log('image: ', image);
+      this.image = image.dataUrl;
+      const blob = this.dataURLtoBlob(image.dataUrl);
+      // SEND REQUEST HERE
+
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  dataURLtoBlob(dataurl: any) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+      bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while (n--) {
+      u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], { type: mime });
+  }
+
 
 }
