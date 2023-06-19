b4f02f611678880e7076e55f2888d3681cc7af1e
Kenneth Manuel Lieyanto <kennethlieyanto99@gmail.com>
2023-06-19 23:49:56 +0700

    Rec data json update, use dynamic page in rec tab

--- src/app/pages/tabRecyclableWaste/tabRecyclableWaste.page.ts
+++ src/app/pages/tabRecyclableWaste/tabRecyclableWaste.page.ts
@@ -1,9 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-tabRecyclableWaste',
   templateUrl: 'tabRecyclableWaste.page.html',
   styleUrls: ['tabRecyclableWaste.page.scss']
 })
 export class TabRecyclableWastePage {
-  constructor() { }
+  recyclables: any[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getJSON().subscribe((data: any[]) => {
+      this.recyclables = data;
+    });
+    console.log(this.recyclables);
+  }
+
+  getJSON(): Observable<any> {
+    return this.http.get("./assets/recyclable-data.json");
+  }
 }
